9bd9ebdc76dbe12059be2136d822905b21ae3f2a
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 12:31:51 +1300

    Update selfupdate.go

--- cmd/jiri/selfupdate.go
+++ cmd/jiri/selfupdate.go
@@ -7,8 +7,8 @@ package main
 import (
 	"fmt"
 
-	"fuchsia.googlesource.com/jiri"
-	"fuchsia.googlesource.com/jiri/cmdline"
+	"github.com/dahlia-os/jiri"
+	"github.com/dahlia-os/jiri/cmdline"
 )
 
 // cmdSelfUpdate represents the "jiri update" command.
