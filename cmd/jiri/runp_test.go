48c56c4f061fa054c0cb328f71d82155fbe63a2a
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 12:31:06 +1300

    Update runp_test.go

--- cmd/jiri/runp_test.go
+++ cmd/jiri/runp_test.go
@@ -11,9 +11,9 @@ import (
 	"strings"
 	"testing"
 
-	"fuchsia.googlesource.com/jiri/gitutil"
-	"fuchsia.googlesource.com/jiri/jiritest"
-	"fuchsia.googlesource.com/jiri/project"
+	"github.com/dahlia-os/jiri/gitutil"
+	"github.com/dahlia-os/jiri/jiritest"
+	"github.com/dahlia-os/jiri/project"
 )
 
 func setDefaultRunpFlags() {
