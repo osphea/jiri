0051094626602e58966813d85d93c7284ec4c31f
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 12:41:24 +1300

    Update upload_test.go

--- cmd/jiri/upload_test.go
+++ cmd/jiri/upload_test.go
@@ -12,11 +12,11 @@ import (
 	"strconv"
 	"testing"
 
-	"fuchsia.googlesource.com/jiri"
-	"fuchsia.googlesource.com/jiri/gerrit"
-	"fuchsia.googlesource.com/jiri/gitutil"
-	"fuchsia.googlesource.com/jiri/jiritest"
-	"fuchsia.googlesource.com/jiri/project"
+	"github.com/dahlia-os/jiri"
+	"github.com/dahlia-os/jiri/gerrit"
+	"github.com/dahlia-os/jiri/gitutil"
+	"github.com/dahlia-os/jiri/jiritest"
+	"github.com/dahlia-os/jiri/project"
 )
 
 func projectName(i int) string {
