3d4e190c17c0c1aaba67add823671df8e076c08e
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 12:50:17 +1300

    Update nested.go

--- cmdline/testdata/nested.go
+++ cmdline/testdata/nested.go
@@ -4,7 +4,7 @@
 
 package main
 
-import "fuchsia.googlesource.com/jiri/cmdline"
+import "github.com/dahlia-os/jiri/cmdline"
 
 // cmdNested represents the nested command.
 var cmdNested = &cmdline.Command{
