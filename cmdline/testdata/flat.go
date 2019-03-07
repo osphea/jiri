d17e4750690bd19d136d3a7115586085cc99e716
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 12:49:22 +1300

    Update flat.go

--- cmdline/testdata/flat.go
+++ cmdline/testdata/flat.go
@@ -4,7 +4,7 @@
 
 package main
 
-import "fuchsia.googlesource.com/jiri/cmdline"
+import "github.com/dahlia-os/jiri/cmdline"
 
 // cmdFlat represents the flat command.
 var cmdFlat = &cmdline.Command{
