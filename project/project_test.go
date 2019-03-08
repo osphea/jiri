8d15eab1205505a796b4ceccefbe2eb00e7989c6
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-08 13:13:00 +1300

    Update project_test.go

--- project/project_test.go
+++ project/project_test.go
@@ -18,11 +18,11 @@ import (
 	"strings"
 	"testing"
 
-	"fuchsia.googlesource.com/jiri"
-	"fuchsia.googlesource.com/jiri/cipd"
-	"fuchsia.googlesource.com/jiri/gitutil"
-	"fuchsia.googlesource.com/jiri/jiritest"
-	"fuchsia.googlesource.com/jiri/project"
+	"github.com/dahlia-os/jiri"
+	"github.com/dahlia-os/jiri/cipd"
+	"github.com/dahlia-os/jiri/gitutil"
+	"github.com/dahlia-os/jiri/jiritest"
+	"github.com/dahlia-os/jiri/project"
 )
 
 func dirExists(dirname string) error {
